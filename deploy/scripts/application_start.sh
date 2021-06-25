f91745fdd942c5480ea4a78096b6dd5af4b01a48
Isaque Costa <isaquebc@gmail.com>
2021-06-25 08:00:07 -0300

    Adding deploy files

---
+++ deploy/scripts/application_start.sh
@@ -0,0 +1,7 @@
+/home/ubuntu/gt/env.sh
+
+cd /home/ubuntu/ida-authentication-api/
+npm install
+
+sudo systemctl daemon-reload
+sudo systemctl start ida-authentication-api
---
+++ deploy/scripts/before_install.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 14
+
+rm -rf /home/ubuntu/ida-authentication-api/dist/*
+
+
+rm -rf /lib/systemd/system/ida-authentication-api.service
