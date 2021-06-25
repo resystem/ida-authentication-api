7ddabdc007b67ca468518c8b274700d30addb311
Isaque Costa <isaquebc@gmail.com>
2021-06-25 10:06:23 -0300

    Finishing nginx config

--- deploy/scripts/before_install.sh
+++ deploy/scripts/before_install.sh
@@ -7,6 +7,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install 14
 
 rm -rf /home/ubuntu/ida-authentication-api/dist/*
+rm /etc/nginx/sites-enabled/default
 
 
 rm -rf /lib/systemd/system/ida-authentication-api.service
