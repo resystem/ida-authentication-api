4fc5957e0f083ee959f1e9154ede16cd75e700f6
Isaque Costa <isaquebc@gmail.com>
2021-07-05 19:43:45 -0300

    Fixing cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ServerModule } from './server.module';
 
 const bootstrap = async () => {
-  const server = await NestFactory.create(ServerModule, { cors: false });
+  const server = await NestFactory.create(ServerModule, { cors: { origin: true, credentials: true } });
   await server.listen(3000);
 };
 
